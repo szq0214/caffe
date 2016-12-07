efa80f9648a398370419ab22043c9702963e016a
szq0214 <zhiqiangshen0214@gmail.com>
2016-12-07 22:34:24 +0800

    init commit

---
+++ matlab/demo/classification_demo.m
@@ -0,0 +1,145 @@
+function [scores, maxlabel] = classification_demo(im, use_gpu)
+% [scores, maxlabel] = classification_demo(im, use_gpu)
+%
+% Image classification demo using BVLC CaffeNet.
+%
+% IMPORTANT: before you run this demo, you should download BVLC CaffeNet
+% from Model Zoo (http://caffe.berkeleyvision.org/model_zoo.html)
+%
+% ****************************************************************************
+% For detailed documentation and usage on Caffe's Matlab interface, please
+% refer to Caffe Interface Tutorial at
+% http://caffe.berkeleyvision.org/tutorial/interfaces.html#matlab
+% ****************************************************************************
+%
+% input
+%   im       color image as uint8 HxWx3
+%   use_gpu  1 to use the GPU, 0 to use the CPU
+%
+% output
+%   scores   1000-dimensional ILSVRC score vector
+%   maxlabel the label of the highest score
+%
+% You may need to do the following before you start matlab:
+%  $ export LD_LIBRARY_PATH=/opt/intel/mkl/lib/intel64:/usr/local/cuda-5.5/lib64
+%  $ export LD_PRELOAD=/usr/lib/x86_64-linux-gnu/libstdc++.so.6
+% Or the equivalent based on where things are installed on your system
+%
+% Usage:
+%  im = imread('../../examples/images/cat.jpg');
+%  scores = classification_demo(im, 1);
+%  [score, class] = max(scores);
+% Five things to be aware of:
+%   caffe uses row-major order
+%   matlab uses column-major order
+%   caffe uses BGR color channel order
+%   matlab uses RGB color channel order
+%   images need to have the data mean subtracted
+
+% Data coming in from matlab needs to be in the order
+%   [width, height, channels, images]
+% where width is the fastest dimension.
+% Here is the rough matlab for putting image data into the correct
+% format:
+%   % convert from uint8 to single
+%   im = single(im);
+%   % reshape to a fixed size (e.g., 227x227)
+%   im = imresize(im, [IMAGE_DIM IMAGE_DIM], 'bilinear');
+%   % permute from RGB to BGR and subtract the data mean (already in BGR)
+%   im = im(:,:,[3 2 1]) - data_mean;
+%   % flip width and height to make width the fastest dimension
+%   im = permute(im, [2 1 3]);
+
+% If you have multiple images, cat them with cat(4, ...)
+
+% Add caffe/matlab to you Matlab search PATH to use matcaffe
+if exist('../+caffe', 'dir')
+  addpath('..');
+else
+  error('Please run this demo from caffe/matlab/demo');
+end
+
+% Set caffe mode
+if exist('use_gpu', 'var') && use_gpu
+  caffe.set_mode_gpu();
+  gpu_id = 0;  % we will use the first gpu in this demo
+  caffe.set_device(gpu_id);
+else
+  caffe.set_mode_cpu();
+end
+
+% Initialize the network using BVLC CaffeNet for image classification
+% Weights (parameter) file needs to be downloaded from Model Zoo.
+model_dir = '../../models/bvlc_reference_caffenet/';
+net_model = [model_dir 'deploy.prototxt'];
+net_weights = [model_dir 'bvlc_reference_caffenet.caffemodel'];
+phase = 'test'; % run with phase test (so that dropout isn't applied)
+if ~exist(net_weights, 'file')
+  error('Please download CaffeNet from Model Zoo before you run this demo');
+end
+
+% Initialize a network
+net = caffe.Net(net_model, net_weights, phase);
+
+if nargin < 1
+  % For demo purposes we will use the cat image
+  fprintf('using caffe/examples/images/cat.jpg as input image\n');
+  im = imread('../../examples/images/cat.jpg');
+end
+
+% prepare oversampled input
+% input_data is Height x Width x Channel x Num
+tic;
+input_data = {prepare_image(im)};
+toc;
+
+% do forward pass to get scores
+% scores are now Width x Height x Channels x Num
+tic;
+% The net forward function. It takes in a cell array of N-D arrays
+% (where N == 4 here) containing data of input blob(s) and outputs a cell
+% array containing data from output blob(s)
+scores = net.forward(input_data);
+toc;
+
+scores = scores{1};
+size(scores)
+scores = squeeze(scores);
+scores = mean(scores,2);
+
+[~,maxlabel] = max(scores);
+
+% call caffe.reset_all() to reset caffe
+caffe.reset_all();
+
+% ------------------------------------------------------------------------
+function images = prepare_image(im)
+% ------------------------------------------------------------------------
+d = load('../+caffe/imagenet/ilsvrc_2012_mean.mat');
+IMAGE_MEAN = d.image_mean;
+IMAGE_DIM = 256;
+CROPPED_DIM = 227;
+
+% resize to fixed input size
+im = single(im);
+im = imresize(im, [IMAGE_DIM IMAGE_DIM], 'bilinear');
+% permute from RGB to BGR (IMAGE_MEAN is already BGR)
+im = im(:,:,[3 2 1]) - IMAGE_MEAN;
+
+% oversample (4 corners, center, and their x-axis flips)
+images = zeros(CROPPED_DIM, CROPPED_DIM, 3, 10, 'single');
+indices = [0 IMAGE_DIM-CROPPED_DIM] + 1;
+curr = 1;
+for i = indices
+  for j = indices
+    images(:, :, :, curr) = ...
+      permute(im(i:i+CROPPED_DIM-1, j:j+CROPPED_DIM-1, :), [2 1 3]);
+    images(:, :, :, curr+5) = images(end:-1:1, :, :, curr);
+    curr = curr + 1;
+  end
+end
+center = floor(indices(2) / 2)+1;
+images(:,:,:,5) = ...
+  permute(im(center:center+CROPPED_DIM-1,center:center+CROPPED_DIM-1,:), ...
+  [2 1 3]);
+images(:,:,:,10) = images(end:-1:1, :, :, curr);
